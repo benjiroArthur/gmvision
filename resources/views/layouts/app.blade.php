94d88a861d40e73d419b513dc313950cee88d46c
Benjamin-Arthur <68535684+Benjamin-Arthur@users.noreply.github.com>
2021-02-18 16:45:24 +0000

    prodfile added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,15 +10,15 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js', config('env') === 'production') }}" defer type="text/javascript"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css', config('env') === 'production') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/app.css', config('env') === 'production') }}" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div id="app">
